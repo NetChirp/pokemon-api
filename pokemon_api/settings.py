0e80f76fcd44ed8d487767ad0f6cf86727d7bd0d
Netchirp <jinxynet@duck.com>
2025-12-05 23:29:23 +0100

    refactor(settings.py): Remove database settings

--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -72,12 +72,14 @@ WSGI_APPLICATION = 'pokemon_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
 # Password validation
